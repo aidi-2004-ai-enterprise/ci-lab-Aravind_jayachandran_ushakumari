11d09c313a7a10e6308cf71ea54553819e5858ee
Aravind J U <aravindju1@gmail.com>
2025-06-11 17:31:30 -0400

    Add unit tests and update CI pipeline

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def load_penguin_data():
     df = pd.read_csv(url)
     return df.shape
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     shape = load_penguin_data()
     print(f"Penguin dataset shape: {shape}")
